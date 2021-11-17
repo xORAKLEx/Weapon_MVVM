672a4a9510f504f88a82f58df3dbf4e4099ecc8e
ORAKLE <kolyan.mikolenko@mail.ru>
2021-11-17 21:01:13 +0200

    Add DAL

---
+++ DAL/Models/WeaponDLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class WeaponDLL
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int AmmoCount { get; set; }
+
+        public string AmmoSize { get; set; }
+    }
+}
---
+++ DAL/Serializers/WeaponSerializer.cs
@@ -0,0 +1,38 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Serializers
+{
+    public class WeaponSerializer
+    {
+        
+        public void Serialize(List<WeaponDLL> weapons)
+        {
+            var formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("weapons.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, weapons);
+            }
+        }
+
+        public List<WeaponDLL> Deserialize()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("weapons.dat", FileMode.OpenOrCreate))
+            {
+                var weapons = (List<WeaponDLL>)formatter.Deserialize(fs);
+
+                return weapons ?? new List<WeaponDLL>();
+            }
+        }
+
+    }
+}
